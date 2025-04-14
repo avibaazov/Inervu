92ae14a182da98315288bed375259c96693b08f4
avi baazov <avibaazov2@gmail.com>
2025-04-14 16:56:46 +0300

    little change3

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   eslint: { ignoreDuringBuilds: true },
-  typescript: { ignoreDuringBuilds: true },
+  typescript: { ignoreBuildErrors: true },
 };
 
 export default nextConfig;
